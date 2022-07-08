b72cbaa93f5b749aeeb435a95026b6ac9e2db402
worashf <uppert83@gmail.com>
2022-07-08 18:36:29 +0300

    add  navigation and category components

---
+++ src/components/Navbar.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  const links = [
+    {
+      id: 1,
+      path: '/',
+      text: 'Books',
+    },
+    {
+      id: 2,
+      path: '/categories',
+      text: 'Categories',
+    },
+  ];
+
+  return (
+    <header className="nav-bar">
+      <Link to="/" className="logo">
+        BookStore CMS
+      </Link>
+      <nav>
+        <ul className="nav">
+          {links.map((link) => (
+            <li key={link.id}>
+              <Link to={link.path}>{link.text}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
