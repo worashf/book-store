c8e102d7205587687dd884dc686e4fefac8ddcf3
worashf <uppert83@gmail.com>
2022-07-08 18:35:00 +0300

    add routing to components

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './style.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import BookPage from './pages/Book.Page';
+import NoFound from './pages/NoFound';
+import Categories from './components/Categories';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<BookPage />} />
+        <Route path="/categories" element={<Categories />} />
+        <Route path="*" element={<NoFound />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
