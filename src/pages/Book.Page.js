31fb34513278b6019146902726f5d152ebcc77d2
worashf <uppert83@gmail.com>
2022-07-08 18:37:08 +0300

    add book page and not found page

---
+++ src/pages/Book.Page.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import BookFrom from '../components/Book.Form';
+import BookList from '../components/Book.List';
+
+const BookPage = () => (
+  <>
+    <BookList />
+    <BookFrom />
+  </>
+);
+
+export default BookPage;
---
+++ src/pages/NoFound.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const NoFound = () => {
+  const heading = <h3>The path is not found</h3>;
+  return <div>{heading}</div>;
+};
+
+export default NoFound;
