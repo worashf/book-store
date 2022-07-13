53b73899e197f0def1ab7c1f05542c02eb86759f
worashf <uppert83@gmail.com>
2022-07-13 16:42:31 +0300

    add new book to store

--- src/components/Book.Form.js
+++ src/components/Book.Form.js
@@ -1,12 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../redux/books/books';
 
-const BookFrom = () => (
-  <form className="book-form">
-    <h2> Add New Book</h2>
-    <input type="text" name="title" placeholder="Book Title" />
-    <input type="text" name="author" placeholder="Author" />
-    <button type="button">Add Book</button>
-  </form>
-);
+const BookFrom = () => {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const id = Math.random();
+    dispatch(addBook(id, title, author));
+    setTitle((e.target.title.value = ''));
+    setAuthor((e.target.author.value = ''));
+  };
+
+  return (
+    <form className="book-form" onSubmit={handleSubmit}>
+      <h2> Add New Book</h2>
+      <input
+        type="text"
+        name="title"
+        placeholder="Book Title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <input
+        type="text"
+        name="author"
+        placeholder="Author"
+        value={author}
+        onChange={(e) => setAuthor(e.target.value)}
+      />
+      <button type="submit">Add Book</button>
+    </form>
+  );
+};
 
 export default BookFrom;
