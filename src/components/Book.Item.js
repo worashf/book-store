c865ee984ed3039411c6d9cc5433282fe0a77e01
worashf <uppert83@gmail.com>
2022-07-13 16:44:16 +0300

    get books to componets

--- src/components/Book.Item.js
+++ src/components/Book.Item.js
@@ -1,31 +1,46 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/books';
 
-const BookItem = () => (
-  <div className="book-info">
-    <div className="info">
-      <p>Book categories</p>
-      <h3>Book Title</h3>
-      <p> Book Author</p>
-      <div className="book-btn">
-        <button type="button"> Comments</button>
-        <button type="button">Remove</button>
-        <button type="button">Edit</button>
+const BookItem = ({ id, title, author }) => {
+  const dispatch = useDispatch();
+
+  return (
+    <div className="book-info">
+      <div className="info">
+        <p>Book categories</p>
+        <h3 style={{ color: 'red' }}>{title}</h3>
+        <p>{author}</p>
+        <div className="book-btn">
+          <button type="button"> Comments</button>
+          <button type="button" onClick={() => dispatch(removeBook(id))}>
+            Remove
+          </button>
+          <button type="button">Edit</button>
+        </div>
+      </div>
+      <div className="progress">
+        <div className="progress-icon" />
+        <div className="status">
+          <h4>100%</h4>
+          <p>Completed</p>
+        </div>
+      </div>
+
+      <div className="chapter">
+        <p>Current chapter</p>
+        <h4>Chapter</h4>
+        <button type="button">Update progress</button>
       </div>
     </div>
-    <div className="progress">
-      <div className="progress-icon" />
-      <div className="status">
-        <h4>100%</h4>
-        <p>Completed</p>
-      </div>
-    </div>
-
-    <div className="chapter">
-      <p>Current chapter</p>
-      <h4>Chapter</h4>
-      <button type="button">Update progress</button>
-    </div>
-  </div>
-);
+  );
+};
 
 export default BookItem;
+
+BookItem.propTypes = {
+  id: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+};
--- src/components/Book.List.js
+++ src/components/Book.List.js
@@ -1,6 +1,23 @@
 import React from 'react';
+
+import { useSelector } from 'react-redux';
 import BookItem from './Book.Item';
 
-const BookList = () => <BookItem />;
+const BookList = () => {
+  const books = useSelector((state) => state.books);
+
+  return (
+    <>
+      {books.map((book) => (
+        <BookItem
+          title={book.title}
+          author={book.author}
+          key={book.id}
+          id={book.id}
+        />
+      ))}
+    </>
+  );
+};
 
 export default BookList;
