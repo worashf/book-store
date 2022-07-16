6388ebe7b6a78b1943a692e06c52ca2aaa430524
worashf <uppert83@gmail.com>
2022-07-16 11:30:10 +0300

    add header styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,36 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FaUser } from 'react-icons/fa';
+import './navBar.css';
 
-const Navbar = () => {
-  const links = [
-    {
-      id: 1,
-      path: '/',
-      text: 'Books',
-    },
-    {
-      id: 2,
-      path: '/categories',
-      text: 'Categories',
-    },
-  ];
-
-  return (
-    <header className="nav-bar">
-      <Link to="/" className="logo">
-        BookStore CMS
-      </Link>
-      <nav>
-        <ul className="nav">
-          {links.map((link) => (
-            <li key={link.id} className="nav-item">
-              <Link to={link.path}>{link.text}</Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </header>
-  );
-};
+const Navbar = () => (
+  <header className="nav-bar">
+    <Link to="/" className="logo">
+      Bookstore CMS
+    </Link>
+    <ul className="nav">
+      <li key="1" className="nav-item">
+        <Link to="/" className="nav-link active">
+          BOOKS
+        </Link>
+      </li>
+      <li key="2" className="nav-item">
+        <Link to="/categories" className="nav-link">
+          CATEGORIES
+        </Link>
+      </li>
+    </ul>
+    <button className="user-icon-container" type="button">
+      <FaUser className="user-icon" />
+    </button>
+  </header>
+);
 
 export default Navbar;
